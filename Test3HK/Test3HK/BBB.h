c75f02c54c66bb40f70ed7995c0b6d8b911fee88
XiaoKai <767713227@qq.com>
2019-12-22 16:57:05 +0800

    B在BBB_dev分支写代码

---
+++ Test3HK/Test3HK/BBB.h
@@ -0,0 +1,17 @@
+//
+//  BBB.h
+//  Test3HK
+//
+//  Created by 王恒凯 on 2019/12/22.
+//  Copyright © 2019 Defence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface BBB : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Test3HK/Test3HK/BBB.m
@@ -0,0 +1,13 @@
+//
+//  BBB.m
+//  Test3HK
+//
+//  Created by 王恒凯 on 2019/12/22.
+//  Copyright © 2019 Defence. All rights reserved.
+//
+
+#import "BBB.h"
+
+@implementation BBB
+
+@end
